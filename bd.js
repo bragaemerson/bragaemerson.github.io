48f995243a50914d20304d82b95f5ebd573bd6ac
bragaemerson <muilokomota@gmail.com>
2022-11-03 12:07:22 -0300

    adicionando banco de dados

--- bd.js
+++ bd.js
@@ -1,14 +1,15 @@
 var bd = openDatabase("meuBD", "1.0", "Meu Banco de dados", 4080);
 
 bd.transaction(function (criar) {
-    criar.executeSql("CREATE TABLE formularios (nome TEXT) ");
+    criar.executeSql("CREATE TABLE formularios (nome TEXT , idade INTEGER) ");
 });
-function salvarNome() {
+function salvarInfo() {
     const nomeUsuario = document.getElementById("nome-usuario").value;
+    const idadeUsuario = parseInt(document.getElementById("idade-usuario").value)
 
     bd.transaction(function (inserir) {
-        inserir.executeSql("INSERT INTO formularios (nome) VALUES (?)", [
-            nomeUsuario,
+        inserir.executeSql("INSERT INTO formularios (nome, idade) VALUES (?, ?)", [
+            nomeUsuario,idadeUsuario
         ]);
     });
 }
