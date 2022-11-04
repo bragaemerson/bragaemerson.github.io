7c5639e324057898213edbea1a54c234b3d6c843
bragaemerson <muilokomota@gmail.com>
2022-11-04 11:52:09 -0300

    adicionando alteracoes de bancos de dados

--- bd.js
+++ bd.js
@@ -5,11 +5,28 @@ bd.transaction(function (criar) {
 });
 function salvarInfo() {
     const nomeUsuario = document.getElementById("nome-usuario").value;
-    const idadeUsuario = parseInt(document.getElementById("idade-usuario").value)
+    const idadeUsuario = parseInt(
+        document.getElementById("idade-usuario").value
+    );
 
     bd.transaction(function (inserir) {
-        inserir.executeSql("INSERT INTO formularios (nome, idade) VALUES (?, ?)", [
-            nomeUsuario,idadeUsuario
-        ]);
+        inserir.executeSql(
+            "INSERT INTO formularios (nome, idade) VALUES (?, ?)",
+            [nomeUsuario, idadeUsuario]
+        );
+    });
+}
+function pesquisaPorNome() {
+    const nomeUsuario = document.getElementById("pesquisa-nome-usuario").value;
+    bd.transaction(function (ler) {
+        ler.executeSql(
+            `SELECT * FROM formularios WHERE nome="${nomeUsuario}"`,
+            [],
+            function (ler, resultados) {
+                const tamanho = resultados.rows.length;
+                const msg = tamanho + "linhas encontradas";
+                console.log(msg);
+            }
+        );
     });
 }
