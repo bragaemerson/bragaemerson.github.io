fa0bb3fa2711b0d4c64f1d9d40869b1a5b95d5a5
bragaemerson <muilokomota@gmail.com>
2022-11-08 11:41:19 -0300

    alteracao novos dados

--- bd.js
+++ bd.js
@@ -4,10 +4,16 @@ bd.transaction(function (criar) {
     criar.executeSql("CREATE TABLE formularios (nome TEXT , idade INTEGER) ");
 });
 function salvarInfo() {
-    const nomeUsuario = document.getElementById("nome-usuario").value;
+    const nomeUsuario = document
+        .getElementById("nome-usuario")
+        .value.toUpperCase();
     const idadeUsuario = parseInt(
         document.getElementById("idade-usuario").value
     );
+    if (nomeUsuario === "" || isNaN(idadeUsuario)) {
+        alert("Falta informacoes!");
+        return false;
+    }
 
     bd.transaction(function (inserir) {
         inserir.executeSql(
@@ -15,17 +21,50 @@ function salvarInfo() {
             [nomeUsuario, idadeUsuario]
         );
     });
+
+    document.getElementById("nome-usuario").value = "";
+    document.getElementById("idade-usuario").value = "";
 }
+
 function pesquisaPorNome() {
-    const nomeUsuario = document.getElementById("pesquisa-nome-usuario").value;
+    const nomeUsuario = document
+        .getElementById("pesquisa-nome-usuario")
+        .value.toUpperCase();
     bd.transaction(function (ler) {
         ler.executeSql(
-            `SELECT * FROM formularios WHERE nome="${nomeUsuario}"`,
+            `SELECT * FROM formularios WHERE nome LIKE "%${nomeUsuario}%"`,
             [],
             function (ler, resultados) {
                 const tamanho = resultados.rows.length;
+
                 const msg = tamanho + "linhas encontradas";
                 console.log(msg);
+
+                const nome = resultados.rows.item(tamanho - 1).nome;
+                const idade = resultados.rows.item(tamanho - 1).idade;
+                document.getElementById("pesquisa-nome-usuario").value = nome;
+                document.getElementById("resultado-pesquisa").value = idade;
+            }
+        );
+    });
+}
+
+function exibeBD() {
+    bd.transaction(function (exibe) {
+        exibe.executeSql(
+            "SELECT * FROM formularios",
+            [],
+            function (exibe, resultados) {
+                
+                const tamanho = resultados.rows.length;
+                let item;
+                document.getElementById("lista-bd").innerHTML = "";
+                for (let i = 0; i < tamanho; i++) {
+                    item = resultados.rows.item(i);
+                    document.getElementById(
+                        "lista-bd"
+                    ).innerHTML += `<p>Nome: ${item.nome}, ${item.idade} anos</p>`;
+                }
             }
         );
     });
